a10386b6c36ddbc365419baf552706aeef456a70
rjarrin <jarrin.ricardo@gmail.com>
2024-02-18 14:24:34 -0500

    Completed JS (05_sumAll)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start, finish) {
 
+    // return ERROR if the parameters are not numbers
+    if (typeof start != "number" || typeof finish != "number") return "ERROR";
+    
+    // return ERROR if negative numbers are provided
+    if (start < 0 || finish < 0) return "ERROR";
+    
+    // allows functionality if larger number is provided as the first argument
+    if (start > finish) {
+        let temp = start;
+        start = finish;
+        finish = temp;
+    }
+
+    let sum = 0;
+    
+    for (let i = start; i <= finish; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
