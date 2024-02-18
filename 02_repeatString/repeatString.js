5a3f7009b5ac72ce430f296e54460ddeb367fb53
rjarrin <jarrin.ricardo@gmail.com>
2024-02-18 14:00:23 -0500

    Completed JS (02_repeatString)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, ...args) {
+    // Only proceed if the repeater number is positive
+    if(args[0] < 0) {
+        return "ERROR";
+    }
+    // Initialize holder variable
+    let word = '';
+    // Loop through the word based on the repeater number
+    for (let i = 0; i < args[0]; i++) {
+        word += string;
+    }
+    return word;
 };
 
 // Do not edit below this line
