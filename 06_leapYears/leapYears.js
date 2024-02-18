5229c80a9c76b7da93b8530f9294132567279170
rjarrin <jarrin.ricardo@gmail.com>
2024-02-18 14:30:18 -0500

    Completed JS (06_leapYears)

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+/* Rules:
+- Leap years are divisible by 4
+- Leap years are NOT divisible by 100
+    - Leap years are divisble by 400
+*/
+const leapYears = function(year) {
+    if (year % 4 == 0) {
+        if (year % 100 == 0 && year % 400 != 0) {
+            return false;
+        }
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
