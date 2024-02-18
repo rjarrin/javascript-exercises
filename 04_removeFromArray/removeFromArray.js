78e833e720ff266cf74d771c905f5cc4498151c4
rjarrin <jarrin.ricardo@gmail.com>
2024-02-18 14:08:59 -0500

    Completed JS (04_removeFromArray)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let newArray = [];
+    // Check each element in the array
+    array.forEach(element => {
+        // Only push to new array if the array element is not in the argument(s)
+        if(!args.includes(element)) {
+            newArray.push(element);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
