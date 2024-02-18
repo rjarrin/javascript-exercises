205b3c182e1ef9d0b92867f17789f33a63e5ebc5
rjarrin <jarrin.ricardo@gmail.com>
2024-02-18 14:40:25 -0500

    Completed JS (07_tempConversion)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  // Formula: (x - 32) * (5/9)
+  let result = (temp - 32) * (5/9);
+  return parseFloat(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  // Formula: (x * (9/5)) + 32
+  let result = (temp * (9/5)) + 32;
+  return parseFloat(result.toFixed(1));
 };
 
 // Do not edit below this line
