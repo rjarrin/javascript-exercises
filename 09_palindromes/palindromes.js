bb6618062ea31e7965b7c26e8b838fc23df7da41
rjarrin <jarrin.ricardo@gmail.com>
2024-03-03 18:50:22 -0500

    JS palindromes completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // Remove any punctuation marks from the string and return in lowercase format
+    let newString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    // Reverse the string and compare
+    console.log("ORG: "+ newString);
+    console.log("REV: " + newString.split("").reverse().join(""))
+    if (newString.split("").reverse().join("") != newString) return false;
+    return true;
 };
 
 // Do not edit below this line
